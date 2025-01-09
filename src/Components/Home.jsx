96321dc8341a8cbef1a76ddad05db05c3179afdc
Moreira_Stephane <moreira.s@outlook.com>
2025-01-09 14:36:21 +0100

    acces produit db ok

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,25 @@
-import Hero from './Hero.jsx'
+import Hero from './Hero.jsx';
+import { useState,useEffect } from 'react';
 
 function Home () {
+    const [data, setData] = useState([]);
+    
+        useEffect(() => {
+            // Appeler l'API backend en PHP
+            fetch('http://localhost/backend/backend-piwee/products.php')
+                .then((response) => response.json())
+                .then((data) => setData(data))
+                .catch((error) => console.error('Erreur :', error));
+        }, []);
+        const products = data.map((product) =>
+            <li key={product.id}>{product.name}</li>
+    );
     return (
         <>
             <div>
                 <Hero/>
                 <img src="/img/2BZYfYkT5Zf0RrR4JmfY4.jpg" alt="" />
+                <ul>{products}</ul>
             </div>
         </>
     )
